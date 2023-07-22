3e4c923ed61222a118c8eedabe218d0f396d0cb5
Roberto Esteves <info@robertoesteves.com>
2023-07-21 21:57:03 -0500

    Add files via upload

--- agent.py
+++ agent.py
@@ -13,6 +13,7 @@ import streamlit as st
 class Agent:
     def __init__(self, openai_api_key: str | None = None) -> None:
         # if openai_api_key is None, then it will look the enviroment variable OPENAI_API_KEY
+        openai_api_key = st.secrets["apikey"]
         self.embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
         self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 
